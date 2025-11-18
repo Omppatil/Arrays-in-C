64b3cd4f80f8c3810dab1d2221edb226bd184699
Om Patil <omppatil2002@gmail.com>
2025-11-18 16:36:38 +0530

    new added

--- Sum_and_Average_of_Array.c
+++ Sum_and_Average_of_Array.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int sum(int num[], int size);
+int sum(int arr[], int size);
 
 int main(void) {
-  int num[5];
+  int arr[5];
   printf("Welcome to Sum and Average of Array.\n");
   for(int i = 0; i < 5; i++) {
-    printf("please enter the %dth number: ", i + 1);
-    scanf("%d", &num[i]);
+    printf("please enter the %dth array number: ", i + 1);
+    scanf("%d", &arr[i]);
   }
-    int addition = sum(num, 5);
+    int addition = sum(arr, 5);
     float avg = addition / 5.0;
     printf("Sum: %d and Average: %.2f", addition, avg);
     return 0;
@@ -16,10 +16,10 @@ int main(void) {
   
 
 
-int sum(int num[], int size) {
+int sum(int arr[], int size) {
   int addition = 0;
   for(int i = 0; i < size; i++) {
-    addition += num[i];
+    addition += arr[i];
   }
   return addition;
 }
