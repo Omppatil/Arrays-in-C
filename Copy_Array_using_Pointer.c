ac2c012f2bfdc36191b2faa45afbe2d498d0b02b
Om Patil <omppatil2002@gmail.com>
2025-11-20 22:29:46 +0530

    new added

--- Copy_Array_using_Pointer.c
+++ Copy_Array_using_Pointer.c
@@ -4,22 +4,22 @@ void copy_arr(char *arr, int size, char *new_arr);
 void print_elements(char *arr, int size);
 
 int main(void) {
-  char arr[] = {'A', 'B', 'c', 'd', 'e', 'F'};
-  char new_arr[6];
+  char arr[13] = {'C', ' ', 'P', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g'};
+  char new_arr[13];
 
   printf("Welcome to Coping of array using Pointer.\n\n");
   printf("Original Array: ");
-  print_elements(arr, 6);
+  print_elements(arr, 13);
 
-  copy_arr(arr, 6, new_arr);
+  copy_arr(arr, 13, new_arr);
   printf("\nCopied Array: ");
-  print_elements(new_arr, 6);
+  print_elements(new_arr, 13);
   return 0;
 }
 
 void print_elements(char *arr, int size) {
   for(int i = 0; i < size; i++) {
-    printf(" %c", arr[i]);
+    printf("%c", arr[i]);
   }
 }
 
